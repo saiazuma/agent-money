a89ca50f2f39713a22a6f2d01da77a0207118640
chenjack <irving19751975@gmail.com>
2024-05-27 00:33:46 +0800

    增加註冊按鈕並加入程式碼

--- app/src/main/java/com/example/agentmoney/MainActivity.java
+++ app/src/main/java/com/example/agentmoney/MainActivity.java
@@ -3,8 +3,15 @@ package com.example.agentmoney;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etAccount;
+    private EditText etPassword;
+    private Button btnLogin;
+    private Button btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
